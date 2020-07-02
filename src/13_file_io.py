991ade4a3267ed35b6f2b53f3fe059bbb18351c3
ThomasMcDaniel91 <thomasm9105@gmail.com>
2020-07-01 21:56:49 -0500

    finishing a few more assignment questions.

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+f = open('src\\foo.txt', 'r')
+for line in f:
+    print(line, end='')
+f.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+f = open('src\\bar.txt', 'w', newline='\n')
+f.write('This is the beginning\n')
+f.write("Of my arbitrary content\n")
+f.write("To this file\n")
+f.close()
